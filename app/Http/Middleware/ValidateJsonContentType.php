78e28cfb84a8f300bd2c7e510752c074e5b1c343
Opeyemi Ilesanmi <opeyemi.ilesanmi@vengreso.com>
2023-11-23 14:41:19 +0100

    validate that request and response is app/json

---
+++ app/Http/Middleware/ValidateJsonContentType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ValidateJsonContentType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->header('Content-Type') !== 'application/json') {
+            return response()->json(['error' => 'Unsupported Media Type'], 415);
+        }
+    
+        return $next($request);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Response::macro('json', function ($value = null, $status = 200, array $headers = [], $options = 0) {
+            $headers['Content-Type'] = 'application/json';
+    
+            return response()->json($value, $status, $headers, $options);
+        });
     }
 
     /**
